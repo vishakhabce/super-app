2264499b79240c439ffce42eb9aab31d25fe8eae
Vishakha Kumari <124717316+vishakhabce@users.noreply.github.com>
2023-04-19 21:43:33 +0000

    updated styles

--- src/components/leftContainer.js
+++ src/components/leftContainer.js
@@ -5,11 +5,13 @@ import "./leftContainer.css";
 function LeftContainer() {
   return (
     <>
-        <div class="image-container">
-          <img width="100%" height="100%" src={concert} alt="concert" />
-        </div>
+      <div class="image-container">
         <p className="LoginAccount">Already have an account?</p>
+        <button className="login">login</button>
         <p className="leftBottomContainer">Discover new things on Superapp</p>
+        <img width="100%" height="100%" src={concert} alt="concert" />
+      </div>
+
     </>
   );
 }
--- src/components/registration.js
+++ src/components/registration.js
@@ -14,6 +14,13 @@ function Registration() {
             Email{"   "} <span className="highlight">|{"   "}</span>
             {"   "}Google
           </div>
+          <input placeholder="Name" id="name" className="input"></input>
+          <input placeholder="UserName" id="UserName" className="input"></input>
+          <input placeholder="Email" id="Email" className="input"></input>
+          <input placeholder="Mobile" id="Mobile" className="input"></input>
+
+          <input id="checkbox" type="checkbox"/><label>Share my registration data with Superapp</label>
+          <button className="signup">SIGN UP</button>
           <b className="termcondition">
             By clicking on Sign up. You agree to Superapp{" "}
             <span className="highlight"> Terms and Conditions of Use</span>
